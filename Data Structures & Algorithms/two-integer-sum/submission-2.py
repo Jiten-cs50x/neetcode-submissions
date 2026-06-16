4db15c7a2b36a631030a8c6a3f4d6eac97474bb3
Jiten!!! <jitenkalal321@gmail.com>
2026-06-16 12:23:15 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #take the first number in position 0
+        #subtract that number from target, save the result
+        # if the result is in array return its position and ith position
+
+        i = 0
+
+        while True:
+
+
+            x = target - nums[i]
+
+            if x in nums and nums.index(x) != i:
+                list = [i, nums.index(x)]
+                return sorted(list)
+
+            i += 1
+
+            if i >= len(nums):
+                return -1        
